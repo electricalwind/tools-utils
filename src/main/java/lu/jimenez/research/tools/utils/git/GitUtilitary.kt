7b420a1f3898180617ea04d5d4c4528288774a29
Matthieu Jimenez <matthieu@jimenez.lu>
2016-09-21 16:29:51 +0200

    update

--- src/main/java/lu/jimenez/research/tools/utils/git/GitUtilitary.kt
+++ src/main/java/lu/jimenez/research/tools/utils/git/GitUtilitary.kt
@@ -1,4 +1,22 @@
-
+/*-
+ * #%L
+ * Utilitary
+ * %%
+ * Copyright (C) 2016 Matthieu Jimenez
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package lu.jimenez.research.tools.utils.git
 
 import lu.jimenez.research.tools.utils.diff.DeltaHistory
@@ -99,27 +117,8 @@ class GitUtilitary(val pathToRepo: String) {
                     var alreadyExisting = false
                     listOfCommits.forEach { commit -> if (commit.revCommit == revcom) alreadyExisting = true }
                     if (alreadyExisting) {
-                        start = null;
+                        start = null
 
-/*-
- * #%L
- * Utilitary
- * %%
- * Copyright (C) 2016 Matthieu Jimenez
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
                     } else {
                         start = revcom;
                         head = start.id
